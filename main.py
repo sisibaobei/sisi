a32ff0bb504e86e381e6d17864e0c792ca9746d9
sisibaobei <130119683+sisibaobei@users.noreply.github.com>
2023-05-12 19:47:19 +0800

    Update main.py

--- main.py
+++ main.py
@@ -123,7 +123,7 @@ def send_message(to_user, access_token, region_name, weather, temp, wind_dir, no
     day = localtime().tm_mday
     today = datetime.date(datetime(year=year, month=month, day=day))
     week = week_list[today.isoweekday() % 7]
-    theClass =theClass[today.weekday()]
+    
     # 获取在一起的日子的日期格式
     love_year = int(config["love_date"].split("-")[0])
     love_month = int(config["love_date"].split("-")[1])
